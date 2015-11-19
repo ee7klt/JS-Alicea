5b73151a5b3ee91f78b8284f6748d942e45374c2
ee7klt <kltah1@gmail.com>
2015-11-19 14:46:28 +0800

    12. code execution

--- global/app.js
+++ global/app.js
@@ -1,9 +1,11 @@
-var a = 'hello world';
-console.log(a);
-
-a = undefined;
-if (a === undefined) {
-  console.log ('a is undefined!');
-} else {
-  console.log( ' a is defined!');
+function b() {
+  console.log('Called b');
 }
+
+b();
+
+console.log(a)
+
+var a = 'Hello World'
+
+console.log(a)
