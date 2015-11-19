86bf3c08d53423082398232eca518b79b2f2040b
ee7klt <kltah1@gmail.com>
2015-11-19 15:05:54 +0800

    14. invocation and execution 2

--- global/app.js
+++ global/app.js
@@ -1,9 +1,16 @@
-function b() {
-  console.log('Called b');
-}
 
 function a() {
   b();
+  var c;
 }
 
+function b() {
+  console.log('Called b');
+  var d = 1;
+}
+
+
 a();
+var d = 2;
+
+console.log(d)
