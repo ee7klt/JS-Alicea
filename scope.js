863b4361524fd8dc16597b353e97652ef1c58a5d
ee7klt <kltah1@gmail.com>
2015-11-24 12:10:58 +0800

    16. the scope chain: b sits lexically in a's execution context. OR. b was created by a

--- scope.js
+++ scope.js
@@ -3,12 +3,17 @@
 
 function a() {
 
-  function b() {
+  function b() {   // change lexical env of b by putting it inside a
+    // outer reference of b is now a cuz that's where it's physically sitting
+    // b is created when a runs. i.e. b in created by a
     console.log(myVar);
   }
+
   var myVar = 2;
   b();
 }
 
 var myVar = 1;
-a();
+a();  // returns 2
+b();   // can no longer call b at the global level
+      // uncaught ReferenceError: b is not defined
