ac7f6f0e190876e48fa5dc30488236aa12c7a8b6
ee7klt <kltah1@gmail.com>
2015-11-26 13:47:04 +0800

    28. || operator always returns something, the first thing that coerces to true, or the RHS if both are false

--- coerce.js
+++ coerce.js
@@ -1,6 +1,18 @@
 function greet(name) {
-
+    name = name || '<Your Name Here>'
   console.log('Hello ' + name)
 }
 
 greet()
+
+
+
+// aside: polluting the global scope
+var a = 1
+function foo() {
+  a = 2;
+  b = 1;
+}
+foo()
+console.log(a)
+console.log(b)   // global scope polluted by non-use of var b
