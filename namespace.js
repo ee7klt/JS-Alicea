e9b0ec3d4a504ca73ce1a2326c0632474519b802
ee7klt <kltah1@gmail.com>
2015-11-29 22:37:38 +0800

    32. faking namespace

---
+++ namespace.js
@@ -0,0 +1,15 @@
+var greet = 'Hello!';
+var greet = 'Hola!';
+
+console.log(greet)
+
+
+var english = {};
+var spanish = {};
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola!'
+
+english.greetings={};
+english.greetings.greet = 'Hello again!';
+console.log(english)
