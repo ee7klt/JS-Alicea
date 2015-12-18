28077943c907092944ad75fea06b489fb3a6b917
ee7klt <kltah1@gmail.com>
2015-12-19 07:38:10 +0800

    45. IIFE and safe code. added greet.js to inject greeting in to global context

--- safecode.js
+++ safecode.js
@@ -1,4 +1,9 @@
+// all this code in IIFE () is 'safe'.
+// variables are contained within it
+
 (function(name) {
   var greeting = 'Hello';
   console.log(greeting + ' ' + name);
 }('safecode'))
+
+console.log(greeting)
