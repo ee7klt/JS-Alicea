2c5bf66bf9075fe839ce12a1f0964944e75f9265
ee7klt <kltah1@gmail.com>
2015-12-26 11:21:06 +0800

    47. understanding closures part 2. how does passing primitives by value fit in to this?

--- closure.js
+++ closure.js
@@ -5,7 +5,15 @@ function greet(whattosay) {
     // even after the outer function has popped off the execution stack.
     // this inner function 'closes in' all the outer variables
     // it will access.
+
+    //whattosay = 'a'
+    console.log(this)
     console.log(whattosay + ' ' + name)
+    var a = function() {
+      console.log('inside a')
+      console.log(this)
+    }
+    a()
   }
 }
 
