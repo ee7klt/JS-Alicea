89b58473c518b925204215bb318f9b31fe28541b
ee7klt <kltah1@gmail.com>
2015-12-30 10:53:46 +0800

    callback

--- closurecallback.js
+++ closurecallback.js
@@ -7,3 +7,15 @@ function sayHiLater() {
 }
 
 sayHiLater();
+
+
+function tellMeWhenDone(callback) {
+  var a = 1000;
+  var b = 2000;
+
+  callback();
+}
+
+tellMeWhenDone(function() {
+  console.log("i'm done");
+})
