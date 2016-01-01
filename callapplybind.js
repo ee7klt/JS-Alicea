29a86bf67f41c4810596550d881ae9f8d77ebafe
ee7klt <kltah1@gmail.com>
2016-01-01 09:10:27 +0800

    50. X.bind\(Y\) binds object Y to method X. i.e. whenever the this keyword is used in function X, it will point to object Y

---
+++ callapplybind.js
@@ -0,0 +1,33 @@
+var person = {
+  firstName: 'Alex',
+  lastName: 'Tah',
+  getFullName: function () {
+    var fullname = this.firstName + ' ' + this.lastName;
+    return fullname;
+  }
+}
+
+
+//suppose have a function outside
+// this will fail because 'this' points to global,
+// not the 'person' object
+var logName = function (lang1, lang2) {
+  console.log('Logged: '+ this.getFullName());
+  console.log('Arguments: '+lang1+' '+lang2);
+  console.log('-----------')
+}
+
+// but wouldn't it be nice if can control what 'this' points to?
+
+
+var logNameSuccess = logName.bind(person);
+logNameSuccess('en','es')
+
+
+// can also do
+
+
+var logName2 = function (lang1, lang2) {
+  console.log('Logged: '+ this.getFullName());
+}.bind(person)
+logName2('en','es')
