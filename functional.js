7d20aca61ccb2c51242d7fe9c64c57127e1c11f8
ee7klt <kltah1@gmail.com>
2016-01-09 00:17:02 +0800

    51. currying, closures for checkPastLimit

--- functional.js
+++ functional.js
@@ -17,10 +17,41 @@ function mapForEach(arr, fn) {
     newArr.push(
       fn(arr[i])
     )
-    
+
   }
   return newArr;
 }
 
 
 console.log(mapForEach(arr1, function(x) {return x+3}))
+console.log(mapForEach(arr1, function(x) {return (x > 2)}))
+
+var checkPastLimit = function(limiter, item) {
+  return item > limiter;
+}
+
+// how to use checkPastLimit?
+// it accepts two parameters
+// curry it!
+
+console.log(mapForEach(arr1, checkPastLimit.bind(this,2)))
+
+
+// still not good enough
+// can create a function that takes only limiter as argument??
+
+var checkPastLimit2 = function(limiter) {
+  return checkPastLimit.bind(this,limiter)
+}
+
+console.log(mapForEach(arr1, checkPastLimit2(2)))
+
+// but above seems like cheating cuz still used bind
+// can use closure to by pass bind
+var checkPastLimit3 = function(limiter) {
+  return function(item) {
+    return item > limiter;
+  }
+}
+
+console.log(mapForEach(arr1, checkPastLimit3(2)))
