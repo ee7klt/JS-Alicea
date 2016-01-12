523634d2518dbf87928ddd4bc512ceaf5535fb69
ee7klt <kltah1@gmail.com>
2016-01-12 12:45:57 +0800

    54. understanding the prototype. Obj.__proto__ = Obj2. Sets prototype of Obj to Obj2

---
+++ call.js
@@ -0,0 +1,26 @@
+function Product(name, price) {
+  console.log('Product this = ')
+  console.log(this);
+  this.name = name;
+  this.price = price;
+
+  if (price < 0) {
+    throw RangeError('Cannot create product ' +
+                      this.name + ' with a negative price');
+  }
+}
+
+function Food(name, price) {
+  console.log('Food this = ')
+  console.log(this)
+  Product.call(this, name, price);
+  this.category = 'food';
+}
+
+function Toy(name, price) {
+  Product.call(this, name, price);
+  this.category = 'toy';
+}
+
+var cheese = new Food('feta', 5);
+var fun = new Toy('robot', 40);
---
+++ proto.js
@@ -0,0 +1,35 @@
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
+
+var john = {
+  firstname: 'John',
+  lastname: 'Doe'
+}
+
+// you should never do this. modern browsers allow way to directly access prototype
+// but will slow down performance
+// DEMO only!!!
+john.__proto__   = person;  // will never mistakently type this
+//set object 'john' s prototype to person
+// john inherits from person
+// this mean, if try to execute method on john that does not exists within john
+// engine will look in person.
+console.log(john.getFullName());
+// 'this' in getFullName now refers to john, not to person
+// i.e. whichever object originated the call.
+console.log(john.firstname); // returns John, not Default. Why??
+// because of prototype chain. first looks in john object. when found, stops
+
+
+var jane = {
+  firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+console.log(jane.getFullName());
