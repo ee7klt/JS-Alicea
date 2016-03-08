405561fc378157f20e35a23701f39eb92a2b1ce1
ee7klt <kltah1@gmail.com>
2016-03-08 10:44:27 +0800

    a function as a property of another function, which is called from within the parent function. how weird

--- Greetr.js
+++ Greetr.js
@@ -3,6 +3,8 @@
     return new Greetr.init(firstName, lastName, language);
   }
 
+
+
   var Greetr.init = function(firstName, lastName, language) {
     this.firstName = firstName || '';
     this.lastName = lastName || '';
@@ -12,6 +14,9 @@
     }
 
   }
+
+  Greetr.prototype = {};
+  Greetr.init.prototype = Greetr.prototype;
 
   global.Greetr = global.G$ = Greetr;
 
